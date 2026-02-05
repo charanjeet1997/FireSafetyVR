93e20d8e031d9854eccc918e4beb70dd9b93b35e
Meet-Janiyani <mjaniyani96@gmail.com>
2026-02-05 18:04:08 +0530

    Added Condition for proper spraying of C02

---
+++ Assets/FireSafety/Scripts/SprayScript.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class SprayScript : MonoBehaviour
+{
+    public InputActionReference sprayAction;
+    public ParticleSystem sprayParticles;
+    public XRSocketInteractor socketInteractor;
+    XRGrabInteractable grab;
+
+    void Awake()
+    {
+        grab = GetComponent<XRGrabInteractable>();
+    }
+
+    void Start()
+    {
+        sprayParticles.Stop();    
+    }
+
+    void OnEnable()
+    {
+        sprayAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        sprayAction.action.Disable();
+    }
+
+    void Update()
+    {
+        if(socketInteractor.hasSelection) return; 
+        
+        float h = sprayAction.action.ReadValue<float>();
+        
+        if (!grab.isSelected)
+        { 
+            if(sprayParticles.isPlaying)
+                    sprayParticles.Stop();
+        }
+
+        if (h > 0.1f)
+        {
+            if (!sprayParticles.isPlaying)
+            {
+                sprayParticles.Play();
+            }
+        }
+        else
+        {
+            if (sprayParticles.isPlaying)
+            {
+                sprayParticles.Stop();
+            }
+        }
+        
+    }
+
+
+}
