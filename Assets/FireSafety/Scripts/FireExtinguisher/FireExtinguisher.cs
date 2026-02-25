db2bcd8a12f16aa920e65e2f6a395c4c8745fa11
Meet-Janiyani <mjaniyani96@gmail.com>
2026-02-25 17:52:05 +0530

    Added Real Fire Minor Bug Fix UI Interaction

--- Assets/FireSafety/Scripts/FireExtinguisher/FireExtinguisher.cs
+++ Assets/FireSafety/Scripts/FireExtinguisher/FireExtinguisher.cs
@@ -1,35 +1,108 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FireExtinguisher : MonoBehaviour
 {
-    public float extinguishDelay = 2f;
-
+    [Header("Fire Particles")]
     private ParticleSystem _fireParticles;
-    private bool _isExtinguishing ;
 
+    [Header("Fire Settings")]
+    [SerializeField] private float maxFireIntensity = 100f;
+    [SerializeField] private float extinguishRate = 30f;
+    [SerializeField] private float recoverRate = 8f;
+    [SerializeField] private float destroyDelay = 2f;
+
+    [Header("Visual Settings")] 
+    [SerializeField] private float minFireSize = 0.2f;
+    [SerializeField] private float maxFireSize = 2f;
+    [SerializeField] private float maxFireSpeed = 10f;
+    [SerializeField] private float minFireSpeed = 20f;
+    
+    private float _currentFireIntensity;
+    private bool _isBeingSprayed = false;
+    private bool _isExtinguished = false;
+
+    private ParticleSystem.MainModule fireMain;
+    
     private void Awake()
     {
         _fireParticles = GetComponent<ParticleSystem>();
     }
 
+    private void Start()
+    {
+        _currentFireIntensity = maxFireIntensity;
+        fireMain = _fireParticles.main;
+    }
+
+    private void Update()
+    {
+        if(_isExtinguished) return;
+
+        HandleFire();
+        UpdateFire();
+        
+        _isBeingSprayed = false;
+    }
+
     void OnParticleCollision(GameObject other)
     {
-        if (!_isExtinguishing && other.CompareTag("Smoke"))
+        if (other.CompareTag("Smoke")) 
         {
-            StartCoroutine(StopFire());
+            _isBeingSprayed = true;
         }
     }
 
-    IEnumerator StopFire()
+    void HandleFire()
     {
-        _isExtinguishing = true;
+        if (_isBeingSprayed)
+        {
+            _currentFireIntensity-=extinguishRate*Time.deltaTime;
+        }
+        else
+        {
+            _currentFireIntensity += recoverRate*2*Time.deltaTime;
+        }
+        if (_currentFireIntensity <= 0f && !_isExtinguished)
+        {
+            _isExtinguished = true;
+            StartCoroutine(ExtinguishFire());
+        }
+        
+        _currentFireIntensity =
+            Mathf.Clamp(_currentFireIntensity, 0f, maxFireIntensity);
+    }
 
-        yield return new WaitForSeconds(extinguishDelay);
 
+    void UpdateFire()
+    {
+        float normalized =
+            Mathf.Pow(_currentFireIntensity / maxFireIntensity, 1.5f);
+        
+        fireMain.startSize=
+            Mathf.Lerp(minFireSize, maxFireSize, normalized);
+
+        fireMain.startSpeed =
+            Mathf.Lerp(maxFireSpeed, minFireSpeed,normalized);
+
+        if (_currentFireIntensity < 15f)
+        {
+            fireMain.startLifetime = 0.3f;
+        }
+        else
+        {
+            fireMain.startLifetime = 1f;
+        }
+        
+    }
+    
+    private IEnumerator ExtinguishFire()
+    {
         _fireParticles.Stop();
         
-        yield return new WaitForSeconds(extinguishDelay);
+        yield return new WaitForSeconds(destroyDelay);
         
         Destroy(_fireParticles.gameObject);
     }
